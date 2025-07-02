b3a05d7496326a35a5b91f62e23b656d2b70200b
Garv Gupta <138745425+Debrogliehypo@users.noreply.github.com>
2025-07-02 07:51:51 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,18 @@
+
+
+
+# question 1
+
+x = int(input("enter the first number : "))
+y = int(input("enter the second number : "))
+
+print(f"the addition is : {x+y}")
+print(f"the subtraction is : {x-y}")
+print(f"the multiplication is {x*y} ")
+print(f"the division is: {x/y}")
+
+
+
+
+
+
---
+++ assignment1Q2.py
@@ -0,0 +1,6 @@
+# question 2
+
+a = input("enter your first name : ")
+b = input("enter your last name :  ")
+
+print(f"Hello, {a} {b}! Welcome to the python program.")
